681e6956ee6a3d07d495268aea9bfdaa2e8d5bb8
dongil618 <rlaehddlf618@gmail.com>
2022-08-02 17:28:27 +0900

    Merge branch 'develop' of https://github.com/dongil618/airbnb-clone-BE … …into develop
    * 'develop' of https://github.com/dongil618/airbnb-clone-BE:
      예약 json 포맷 변경
      RoomController 주석삭제
      room 서비스,컨트롤러 주석 삭제
      예약 성공시response수정
      예약 model,dto
    
    # Conflicts:
    # src/main/java/com/hanghae99/airbnbclonebe/controller/RoomController.java

--- src/main/java/com/hanghae99/airbnbclonebe/model/Reservation.java
+++ src/main/java/com/hanghae99/airbnbclonebe/model/Reservation.java
@@ -1,8 +1,16 @@
 package com.hanghae99.airbnbclonebe.model;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
+import com.hanghae99.airbnbclonebe.dto.ReservationRequestDto;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
+
 import javax.persistence.*;
+import java.*;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 
@@ -24,22 +32,48 @@ public class Reservation extends TimeStamped{
     @JoinColumn(name = "USER_ID")
     private User user;
 
+    @JsonDeserialize(using= LocalDateDeserializer.class)
+    @JsonFormat(shape = JsonFormat.Shape.STRING,pattern = "yyyyMMdd")
     @Column(nullable = false)
-    private LocalDateTime checkIn;
+    private LocalDate checkIn;
+
+    @JsonDeserialize(using= LocalDateDeserializer.class)
+    @JsonFormat(shape = JsonFormat.Shape.STRING,pattern = "yyyyMMdd")
+    @Column(nullable = false)
+    private LocalDate checkOut;
 
     @Column(nullable = false)
-    private LocalDateTime checkOut;
-
-    @Column(nullable = false)
-    private int GuestNum;
+    private int guestNum;
 
     @Column(nullable = false)
     private int totalPrice;
+
+    @Column(nullable = false)
+    private boolean status;//예약 상태(가능=TRUE,불가능=FALSE)
 
     @Column(nullable = false)
     private boolean isCancel;
 
     @Column(nullable = false)
     private boolean isComplete;
+
+    //예약생성을 위한 생성자
+    public Reservation(ReservationRequestDto requestDto,Room room,User user)
+    {
+        //예약 요청 requestDto를 받아서, 해당 룸, 해당 유저와 함께 저장
+        this.checkIn=requestDto.getCheckIn();
+        this.checkOut=requestDto.getCheckOut();
+        this.guestNum=requestDto.getGuestNum();
+        this.totalPrice=requestDto.getTotalPrice();
+        this.room=room;
+        this.user=user;
+    }
+
+
+
+
+
+
+
 
 }
--- src/main/java/com/hanghae99/airbnbclonebe/repository/RoomRepository.java
+++ src/main/java/com/hanghae99/airbnbclonebe/repository/RoomRepository.java
@@ -2,7 +2,16 @@ package com.hanghae99.airbnbclonebe.repository;
 
 import com.hanghae99.airbnbclonebe.model.Room;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+
+import java.util.List;
+import java.util.Optional;
+
+
+@Repository
 public interface RoomRepository extends JpaRepository<Room, Long>, RoomRepositoryCustom {
 
+    List<Room> findAllByUserId(Long id);
+    Optional<Room> findRoomById(Long roomId);
 }
