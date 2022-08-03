9c41ef56e7f7df9435cd09fd5fcb3f5e0a24323a
dongil618 <rlaehddlf618@gmail.com>
2022-08-03 17:34:20 +0900

    Revert "Revert "마이페이지(예약 현황), 마이페이지(위시리스트)""
    This reverts commit b8a0e506de0956130d69fdbabf2764ed29e8b430.

---
+++ src/main/java/com/hanghae99/airbnbclonebe/repository/ReservationRepositoryImpl.java
@@ -0,0 +1,64 @@
+package com.hanghae99.airbnbclonebe.repository;
+
+import com.hanghae99.airbnbclonebe.dto.GetReservationsResponseDto;
+import com.hanghae99.airbnbclonebe.model.*;
+import com.querydsl.core.types.ExpressionUtils;
+import com.querydsl.core.types.Projections;
+import com.querydsl.jpa.JPAExpressions;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
+import org.springframework.data.domain.SliceImpl;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+public class ReservationRepositoryImpl implements ReservationRepositoryCustom{
+
+    private final JPAQueryFactory queryFactory;
+
+    private QRoom room = QRoom.room;
+
+    private QImage image = QImage.image;
+    private QReservation reservation = QReservation.reservation;
+
+    @Override
+    public Slice<GetReservationsResponseDto> findAllByUserAndRoomOrderByCreatedAt(Pageable pageable, Long userId) {
+        List<GetReservationsResponseDto> returnReservation = queryFactory.select(Projections.fields(
+                GetReservationsResponseDto.class,
+                        room.id.as("roomId"),
+                        reservation.checkIn,
+                        reservation.checkOut,
+                        reservation.guestNum,
+                        reservation.totalPrice,
+                        room.location,
+                        reservation.status,
+                        reservation.isCancel,
+                        reservation.isComplete,
+                        // imgUrl 1개만 가져오기  room.imageList.get(0) 오류남
+                        ExpressionUtils.as(
+                                JPAExpressions
+                                        .select(image.imgUrl)
+                                        .from(image)
+                                        .where(image.id.eq(
+                                                JPAExpressions
+                                                        .select(image.id.min())
+                                                        .from(image)
+                                                        .where(room.id.eq(image.room.id))
+                                        ))
+                                ,"imgUrl"
+                        )
+                ))
+                .from(reservation)
+                .join(room)
+                .on(reservation.room.id.eq(room.id))
+                .orderBy(reservation.createdAt.desc())
+                .offset(pageable.getOffset())
+                .limit(pageable.getPageSize())
+                .fetch();
+
+        return new SliceImpl<>(returnReservation, pageable, returnReservation.iterator().hasNext());
+
+    }
+}
--- src/main/java/com/hanghae99/airbnbclonebe/repository/RoomRepositoryCustom.java
+++ src/main/java/com/hanghae99/airbnbclonebe/repository/RoomRepositoryCustom.java
@@ -6,5 +6,6 @@ import org.springframework.data.domain.Slice;
 
 public interface RoomRepositoryCustom {
     Slice<GetRoomsResponseDto> findAllByCategoryOrderByCreatedAt(String category, Pageable pageable, Long userId);
+    Slice<GetRoomsResponseDto> findAllByOrderByCreatedAt(Pageable pageable, Long userId);
     Slice<GetRoomsResponseDto> findAllByCategoryOrderByCreatedAtFilter(String category, Pageable pageable, Long userId, boolean parking, boolean kitchen, boolean aircon, boolean wifi, boolean washer, boolean tv, int minPrice, int maxPrice);
 }
