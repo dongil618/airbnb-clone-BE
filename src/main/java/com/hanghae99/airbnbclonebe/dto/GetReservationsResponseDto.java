26107c7df0360cdbd1ed1bb7a501c6ac5c45b70f
dongil618 <rlaehddlf618@gmail.com>
2022-08-04 16:31:09 +0900

    예약현황 에러 해결
    LocalDate -> LocalDateTime으로 수정

--- src/main/java/com/hanghae99/airbnbclonebe/dto/GetReservationsResponseDto.java
+++ src/main/java/com/hanghae99/airbnbclonebe/dto/GetReservationsResponseDto.java
@@ -3,20 +3,20 @@ package com.hanghae99.airbnbclonebe.dto;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 @Getter
 @NoArgsConstructor
 public class GetReservationsResponseDto {
 
     private Long roomId;
-    private LocalDate checkIn;
-    private LocalDate checkOut;
+    private String title;
+    private LocalDateTime checkIn;
+    private LocalDateTime checkOut;
     private int guestNum;
     private int totalPrice;
     private String imgUrl;
     private String location;
     private Boolean isCancel;
     private Boolean isComplete;
-    private Boolean status;
 }
